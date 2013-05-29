28d6bf47e945b995d8dec46a8932c24f02af37fd
Robert Walport <robertwalport@gmail.com>
2013-05-29 13:10:04 -0400

    Switching stored procedures to new database format
    all stored procedures now working for document library! hopefully

--- investor/documents/functions.sql
+++ investor/documents/functions.sql
@@ -1,16 +1,18 @@
 
 -- Gets doc information for investor and tracks it as a viewing of the document
-CREATE or REPLACE FUNCTION document.get_docmetaI(user_id email, docid int) returns setof document.docinfo language plpgsql as $$
-begin 
-  perform distinct doc_id from document.docshares where sent_to=user_id and doc_id=docid;
-  if not found then return query select * from document.docinfo where 1 = 0; end if;
-  insert into document.docshares(doc_id, sent_to, activity) values (docid, user_id, 'viewed');
-  return query select * from document.docinfo where doc_id=docid;
+CREATE or REPLACE FUNCTION document.get_docmetaI(docid int) returns setof document.investor_docinfo language plpgsql as $$
+begin
+  perform distinct doc_id from document.my_investor_revoked where doc_id=docid;
+  if found then return query select * from document.investor_docinfo where 1 = 0; end if;
+  perform distinct doc_id from document.my_investor_shares where doc_id=docid;
+  if not found then return query select * from document.investor_docinfo where 1 = 0; end if;
+  insert into document.my_shares(doc_id, sent_to, sender, sent_from, activity) values (docid, current_user, current_user, (select distinct sent_from from document.my_investor_shares where docid=doc_id), 'viewed');
+  return query select * from document.investor_docinfo where doc_id=docid;
 end $$;
 
 -- Get all the documents shared with an investor for the investor list page
 create type document.investor_list as (doc integer, status document.activity_type, docname character varying, last_updated timestamp);
 
-CREATE OR REPLACE FUNCTION document.get_investordocs(user_id email) returns setof document.investor_list language plpgsql as $$
-begin return query select doc, status, docname, last_updated from document.docinfo i, (select doc_id as doc, max(activity) as status from document.docshares where sent_to=user_id group by doc_id) as d where d.doc=i.doc_id and d.doc not in (SELECT doc_id from document.revoked rev where rev.sent_to=user_id);
+CREATE OR REPLACE FUNCTION document.get_investordocs() returns setof document.investor_list language plpgsql as $$
+begin return query select doc, status, docname, last_updated from document.investor_docinfo i, (select doc_id as doc, max(activity) as status from document.my_investor_shares group by doc_id) as d where d.doc=i.doc_id and d.doc not in (SELECT doc_id from document.my_investor_revoked);
 end $$;
