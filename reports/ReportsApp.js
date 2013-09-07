49b5ae3ce9779c1c12e44692659d50a0cc95174e
Robert M. Lefkowitz <r0ml@mac.com>
2013-09-07 17:13:16 -0400

    moved testing to devops

--- reports/ReportsApp.js
+++ reports/ReportsApp.js
@@ -1,20 +1,43 @@
-var app = angular.module('ReportsApp', ['ui.bootstrap']);
+
+var app = angular.module('ReportsApp', ['ui.bootstrap', 'brijj']);
 
 //this is used to assign the correct template and controller for each URL path
 app.config(function($routeProvider, $locationProvider){
   $locationProvider.html5Mode(true).hashPrefix('');
+  $routeProvider.
+      when('/admin', {templateUrl: '/reports/admin.html',   controller: 'ContactCtrl'}).
+      when('/signups', {templateUrl: '/reports/signups.html', controller: 'ReportsCtrl'}).
+      otherwise({redirectTo: '/' });
 });
 
-function ContactCtrl($scope) {
-  SWBrijj.procm('profile').then(function(x) { 
-    console.log(x);
-    $scope.items = x;
-    $scope.$apply();
+app.controller('ReportsCtrl',['$scope', 'SWBrijj', function($scope, SWBrijj) {
+  $scope.authorize = function() {
+  SWBrijj.proc("get_google_access").then(function(x) {
+    var ru = x[1][0];
+    document.cookie="rux="+encodeURIComponent(ru)+";path=/";
+    document.location.href=ru;
   });
-};
+  }
 
-function SignupCntCtl($scope) {
-  SWBrijj.proc('get_signupcnt_all').then(function(x) { 
+  $scope.download = function() {
+  SWBrijj.getSignups().then(function(x) {
+    document.location.href=x;
+  });
+  }
+}]);
+
+
+
+app.controller('ContactCtrl',['$scope', 'SWBrijj', function($scope, SWBrijj) {
+    SWBrijj.procm('profile').then(function(x) {
+      console.log(x);
+      $scope.items = x;
+      $scope.$apply();
+    });
+}]);
+
+app.controller('SignupCntCtl',['$scope', 'SWBrijj', function($scope, SWBrijj) {
+  SWBrijj.proc('get_signupcnt_all').then(function(x) {
     $scope.signupcnt_all = x[1][0]; 
     $scope.$apply();
   });
@@ -53,4 +76,74 @@ function SignupCntCtl($scope) {
 
     SWBrijj.procd(downloadname, "get_signups", "application/ms-excel", new Date()).then( function(x) { document.location.href=x; });
   };
-};  
+}]);
+
+
+/*
+ <!-- has to go here because it needs to come after jQuery - - which is in foot.inc -->
+ <script src="js/angular.js"></script>
+ <script src="js/ng-infinite-scroll.js"></script>
+
+
+ <script>
+ var nli = function(x) { document.querySelector("#googleResult").innerHTML=x.message;
+ if (x.message == "Not Logged In") {
+ var sc = angular.element(document.querySelector("#login-dialog")).scope();
+ sc.show = true;
+ sc.$apply();
+ }
+ }
+
+ jQuery("#toGoogle").click(function() {
+ var gsn = document.querySelector("input[name=worksheet]").value;
+ SWBrijj.proc("signUpsToGoogle", gsn)
+ .then(function(x) { document.querySelector("#googleResult").innerHTML=x;})
+ .except(nli);
+ });
+
+
+ var LoginController = function($scope) {
+ $scope.connect = function() {
+ SWBrijj.login($scope.username, $scope.password).then(function(data) {
+ if (data) { $scope.show = false; $scope.result=""; } else $scope.result = "Login failed";
+ $scope.$apply();
+ var os = angular.element(document.querySelector("#demoDiv")).scope();
+ os.busy = false;
+ os.nextPage();
+ os.$apply();
+ });
+ };
+ };
+
+ var app = angular.module("myApp",['infinite-scroll']);
+ app.directive("interested", function() {
+ return {
+ // scope: { item: '=' }
+ template: "<div><span>{{item.company_name}} ( <a hg_ref='https://{{item.company_domain}}' > {{item.company_domain}}</a> )</span>"+
+ "<p><span>{{item.name}} ( {{item.email}} )</span><small>created {{item.created}}</small></div>"
+ }
+ });
+  */
+/*
+ app.controller('DemoController', function($scope) {
+ $scope.items = [];
+ $scope.busy = false;
+ $scope.before = new Date();
+
+ $scope.nextPage = function() {
+ if ($scope.busy) return;
+ $scope.busy = true;
+
+ SWBrijj.procm("signUps", 30, $scope.before).then(function(data) {
+ var items = data;
+ for (var i = 0; i < data.length; i++) {
+ $scope.items.push(data[i]);
+ }
+ $scope.before = $scope.items[$scope.items.length - 1].created;
+ $scope.busy = false;
+ $scope.$apply();
+ }).except(nli);
+ };
+ });
+
+    */
--- test/TestApp.js
+++ test/TestApp.js
@@ -1,5 +1,5 @@
 
-var app = angular.module('TestApp', ['ngResource']);
+var app = angular.module('TestApp', ['ngResource', 'brijj']);
 
 //this is used to assign the correct template and controller for each URL path
 app.config(function($routeProvider, $locationProvider){
@@ -7,28 +7,16 @@ app.config(function($routeProvider, $locationProvider){
   // $locationProvider.html5Mode(false).hashPrefix('!');
 
   $routeProvider.
-      when('/google', {controller:GoogleCtrl, templateUrl:'google.html'}).
-      when('/dropbox', {controller:DropboxCtrl, templateUrl: 'dropbox.html'}).
-      when('/linkedin', {controller:LinkedinCtrl, templateUrl: 'linkedin.html'}).
-      when('/twitter', {controller:TwitterCtrl, templateUrl: 'twitter.html'}).
-      when('/upload', {controller:UploadCtrl, templateUrl: 'upload.html'}).
-      when('/other', {controller:OtherCtrl, templateUrl: 'other.html'}).
+      when('/google', {controller:'TestCtrl', templateUrl:'google.html'}).
+      when('/dropbox', {controller:'TestCtrl', templateUrl: 'dropbox.html'}).
+      when('/linkedin', {controller: 'TestCtrl', templateUrl: 'linkedin.html'}).
+      when('/twitter', {controller: 'TestCtrl', templateUrl: 'twitter.html'}).
+      when('/upload', {controller: 'TestCtrl', templateUrl: 'upload.html'}).
+      when('/other', {controller:'TestCtrl', templateUrl: 'other.html'}).
       otherwise({redirectTo:'/other'});
 });
 
-app.controller("MainTestController", function($scope, $location) {
-    $scope.toGoogle = function() { $location.path('google') };
-    $scope.toDropbox = function() { $location.path('dropbox') };
-    $scope.toLinkedin = function() { $location.path('linkedin') };
-    $scope.toTwitter = function() { $location.path('twitter') };
-    $scope.toUpload = function() { $location.path('upload')};
-    $scope.toOther = function() { $location.path('other') };
-    $scope.tab = function(x) { 
-      var p = $location.path();  if (p == '/') p='google';
-      return p == x; };
-} );
-
-function UploadCtrl($scope) {
+app.controller('TestCtrl',['$scope','SWBrijj',function($scope,SWBrijj) {
     // var dropbox = $scope.$element.querySelector(".dropbox"); // $element seems to be an array of elements
     $scope.dropText = 'Drop files here...';
     $scope.files = [];
@@ -77,13 +65,6 @@ function UploadCtrl($scope) {
       // might as well go ahead and upload ( no point in getting a click
       $scope.uploadFile();
     }
-
-    var dropbox = angular.element(".dropbox")[0];
-
-    dropbox.addEventListener("dragenter", dragEnterLeave, false);
-    dropbox.addEventListener("dragleave", dragEnterLeave, false);
-    dropbox.addEventListener("dragover", dragOver, false);
-    dropbox.addEventListener("drop", drop, false);
 
     $scope.setFiles = function (element) {
         $scope.files = [];
@@ -140,27 +121,20 @@ function UploadCtrl($scope) {
         $scope.$apply();
         alert("The upload has been canceled by the user or the browser dropped the connection.")
     }
-}
 
-function GoogleCtrl($scope) {
   $scope.googleList = function () {
       SWBrijj.procm("oauth.get_google_list", $scope.mimeType).then(function (x) { $scope.gfiles = x; $scope.$apply(); });
   };
 
-	// I should check for google access
-}
 
-function TwitterCtrl($scope) {
   $scope.contactSave = function () {
       SWBrijj.proc("contact_update", $scope.name, $scope.street, $scope.city, $scope.state, $scope.postalcode, $scope.country)
         .then(function (x) { alert("saved: "+x);
       });
   };
   //noinspection JSUnresolvedVariable
-    SWBrijj.tbl('account.profile').then(function(x) { initPage($scope, x) }).except(initFail);
-}
+    // SWBrijj.tbl('account.profile').then(function(x) { initPage($scope, x) }).except(initFail);
 
-function DropboxCtrl($scope, $location) {  
   $scope.contactSave = function(){
      SWBrijj.proc("social_update", $scope.twitter, $scope.linkedin, $scope.google, $scope.dropbox, $scope.facebook).
         then(function(x) { alert("done: "+x); });
@@ -184,14 +158,20 @@ function DropboxCtrl($scope, $location) {
   }
 
 
-    SWBrijj.tbl('account.profile').then(function(x) { initPage($scope, x) }).except(initFail);
+  $scope.dropbox_init = function() {
+    // SWBrijj.tbl('account.profile').then(function(x) { initPage($scope, x) }).except(initFail);
     SWBrijj.procm('oauth.dropbox_list','')
       .then(function(x)  { $scope.dropboxFiles=x; $scope.$apply(); })
       .except( function(x) {} );
+    var dropbox = angular.element(".dropbox")[0];
 
-}
+    dropbox.addEventListener("dragenter", dragEnterLeave, false);
+    dropbox.addEventListener("dragleave", dragEnterLeave, false);
+    dropbox.addEventListener("dragover", dragOver, false);
+    dropbox.addEventListener("drop", drop, false);
 
-function LinkedinCtrl($scope) {
+  };
+
     $scope.currentPassword="";
     $scope.newPassword="";
     $scope.passwordConfirm="";
@@ -219,121 +199,5 @@ function LinkedinCtrl($scope) {
             else alert("Oops.  Change failed");
         }).except(function(x) {alert("Oops.  Change failed: "+x); });
     };
-}
 
-function OtherCtrl($scope) {
-    $scope.upload = function () {
-        alert('heh');
-    };
-    
-    SWBrijj.tbl('account.profile').then(function(x) { initPage($scope,x) }).except(initFail);
-}
-
-app.controller("FileDNDCtrl", function($scope, $element) {
-    var dropbox = $element[0].querySelector(".dropbox"); // $element seems to be an array of elements
-    $scope.dropText = 'Drop files here...';
-    $scope.files = [];
-    
-    $scope.fmtFileSize = function (file) {
-        if (file.size > 1024 * 1024 * 1024) return parseFloat(file.size / 1024 / 1024 / 1024).toFixed(2) + " GB";
-        else if (file.size > 1024 * 1024) return parseFloat(file.size / 1024 / 1024).toFixed(2) + " MB";
-        else if (file.size > 1024) return parseFloat(file.size / 1024).toFixed(2) + " kB";
-        else return file.size + " bytes";
-    };
-
-    // init event handlers
-    function dragEnterLeave(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-        $scope.dropText = "Drop files here...";
-        $scope.dropClass = "";
-        $scope.$apply();
-    }
-    
-    function dragOver(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-        var ok = evt.dataTransfer && evt.dataTransfer.types && evt.dataTransfer.types.contains('Files');
-        $scope.dropText = ok ? 'Drop files here...' : 'Only files are allowed!';
-        $scope.dropClass = ok ? 'over' : 'not-available';
-        $scope.$apply();
-    }
-    
-    function drop(evt) {
-        // console.log('drop evt:', JSON.parse(JSON.stringify(evt.dataTransfer)))
-        evt.stopPropagation();
-        evt.preventDefault();
-        $scope.dropText = 'Drop more files here...';
-        $scope.dropClass = '';
-        $scope.$apply();
-        var files = evt.dataTransfer.files;
-        if (files.length > 0) {
-            for (var i = 0; i < files.length; i++) {
-                $scope.files.push(files[i])
-            }
-        }
-        $scope.$apply();
-    }
-    
-    dropbox.addEventListener("dragenter", dragEnterLeave, false);
-    dropbox.addEventListener("dragleave", dragEnterLeave, false);
-    dropbox.addEventListener("dragover", dragOver, false);
-    dropbox.addEventListener("drop", drop, false);
-
-    $scope.setFiles = function(element) {
-        $scope.files = [];
-        for (var i = 0; i < element.files.length; i++) { $scope.files.push(element.files[i]); }
-        $scope.progressVisible = false;
-    };
-
-    $scope.uploadFile = function() {
-        var fd = new FormData();
-        $scope.progressVisible = true;
-        for (var i in $scope.files) fd.append("uploadedFile", $scope.files[i]);
-        SWBrijj.uploadFile(fd).then(function(x) { console.log(x); }).except( function(x) { alert(x); });
-        /*var xhr = new XMLHttpRequest()
-        xhr.upload.addEventListener("progress", uploadProgress, false);
-        xhr.addEventListener("load", uploadComplete, false);
-        xhr.addEventListener("error", uploadFailed, false);
-        xhr.addEventListener("abort", uploadCanceled, false);
-        xhr.open("POST", "/fileupload");
-        xhr.send(fd) */
-    };
-
-    function uploadProgress(evt) {
-        $scope.$apply(function(){
-            if (evt.lengthComputable) {
-                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
-            } else {
-                $scope.progress = 'unable to compute'
-            }
-        })
-    }
-
-    function uploadComplete(evt) {
-        alert(evt.target.responseText)
-    }
-
-    function uploadFailed(evt) {
-        alert("There was an error attempting to upload the file.")
-    }
-
-    function uploadCanceled(evt) {
-        $scope.progressVisible = false;
-        $scope.$apply();
-        alert("The upload has been canceled by the user or the browser dropped the connection.")
-    }
-}
-);
-
-function initPage($scope, x) {
-  var y = x[0]; // the fieldnames
-  var z = x[1]; // the values
-  
-  for(var i=0;i<y.length;i++) { if (z[i] !== null) { $scope[y[i]]=z[i]; } }
-  $scope.$apply();
-}
-
-function initFail(x) {
-  document.location.href='/login';
-}
+}]);
