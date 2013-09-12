314fe3f59cad785d36b91fa42a2a5f2ed274bd36
Robert Walport <robertwalport@gmail.com>
2013-09-12 10:57:23 -0400

    adjusted the reload on the investor page

--- documents/CompanyDocuments.js
+++ documents/CompanyDocuments.js
@@ -122,11 +122,11 @@ docviews.run(function($rootScope, $document) {
  ISSUER CONTROLLERS
  ************************************************************************************************/
 
-docviews.controller('CompanyDocumentListController', ['$scope','$modal','$q', '$rootScope', '$route', 'SWBrijj',
-    function($scope, $modal, $q, $rootScope, $route, SWBrijj) {
+docviews.controller('CompanyDocumentListController', ['$scope','$modal','$q','$location', '$rootScope', '$route', 'SWBrijj',
+    function($scope, $modal, $q, $rootScope, $location, $route, SWBrijj) {
   if ($rootScope.selected.role == 'investor') {
-    // $location.path('/investor-list?'); // goes into a bottomless recursion ?
-    document.location.href='/documents';  // this works
+    $location.path('/investor-list'); // goes into a bottomless recursion ?
+    // document.location.href='/documents';  // this works
     // $location.path('/');
     return;
   }
