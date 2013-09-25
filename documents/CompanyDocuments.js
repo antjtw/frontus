58c4d23169066a6ef1d587401e2b757a84bb82e9
brianru <brianrubinton@gmail.com>
2013-09-25 11:07:13 -0400

    US-1092 - edit document name field, created a new directive to limit length and selectively restrict inputs

--- documents/CompanyDocuments.js
+++ documents/CompanyDocuments.js
@@ -724,6 +724,49 @@ docviews.filter('nameoremail', function () {
     };
 });
 
+docviews.directive('restrictContentEditable', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      // view -> model
+      var ff = function() {
+        scope.$apply(function() {
+          ctrl.$setViewValue(elm.html());
+        });
+        scope.$emit('updated:name');
+      };
+
+      elm.on('blur', ff);
+      elm.bind("keydown keypress", function(event) {
+        var allowedKeys = [8,46,37,38,39,40,27];
+        // backspace, delete, up, down, left, right, esc
+        if (event.which === 13) {
+            // key = enter
+            event.preventDefault();
+            event.currentTarget.blur();
+        } else if (elm.html().length >= 58
+                   && allowedKeys.indexOf(event.which) === -1
+                   && !event.ctrlKey
+                   && !event.metaKey) {
+            // reached maxlength AND entered key is not allowed AND not key combination
+            event.preventDefault();
+        } else if (event.which === 86 && (event.ctrlKey || event.metaKey)) {
+            // disallow paste
+            event.preventDefault();
+        }
+      });
+
+      // model -> view
+      ctrl.$render = function() {
+        elm.html(ctrl.$viewValue);
+      };
+
+      // load init value from DOM
+      ctrl.$setViewValue(elm.html());
+    }
+  };
+});
+
 docviews.directive('contenteditable', function() {
   return {
     require: 'ngModel',
@@ -739,9 +782,10 @@ docviews.directive('contenteditable', function() {
       elm.on('blur', ff);
       elm.bind("keydown keypress", function(event) {
         if (event.which === 13) {
+          // key = enter
           event.preventDefault();
           event.currentTarget.blur();
-        }
+        } 
       });
 
       // model -> view
