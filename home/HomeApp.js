03f02f12135e075ee9a9ddf70c1ed4635a17dc62
Robert Walport <robertwalport@gmail.com>
2013-10-02 16:10:06 -0400

    added tracking on the company home for the captable

--- home/HomeApp.js
+++ home/HomeApp.js
@@ -6,107 +6,116 @@ var app = angular.module('HomeApp', ['ngResource', 'ui.bootstrap', 'ui.event', '
  */
 //this is used to assign the correct template and controller for each URL path
 app.config(function($routeProvider, $locationProvider){
-  $locationProvider.html5Mode(true).hashPrefix('');
+    $locationProvider.html5Mode(true).hashPrefix('');
 
-  $routeProvider.
-      when('/investor', {controller: 'InvestorCtrl', templateUrl:'investor.html'}).
-      when('/company', {controller: 'CompanyCtrl', templateUrl:'company.html'}).
-      otherwise({redirectTo:'/investor'});
+    $routeProvider.
+        when('/investor', {controller: 'InvestorCtrl', templateUrl:'investor.html'}).
+        when('/company', {controller: 'CompanyCtrl', templateUrl:'company.html'}).
+        otherwise({redirectTo:'/investor'});
 });
 
 app.controller('CompanyCtrl', ['$scope','$rootScope','$route','$location', '$routeParams','SWBrijj', 'navState',
-  function($scope, $rootScope, $route, $location, $routeParams, SWBrijj, navState) {
+    function($scope, $rootScope, $route, $location, $routeParams, SWBrijj, navState) {
 
-    if (navState.role == 'investor') {
-        $location.path('/investor');
-        return;
-    }
+        if (navState.role == 'investor') {
+            $location.path('/investor');
+            return;
+        }
 
-  if ($routeParams.msg) {
-    if ($routeParams.msg == "resetPassword") {
-      $scope.$emit("notification:success", "You have successfully changed your password.");
-    }
-  }
+        //Variable for setting the messages on the onboarding
+        $scope.captablestart = "Create";
 
-  $scope.company = navState.name;
-  SWBrijj.tblm('account.onboarding').then(function(x) { 
-    $scope.onboarding = x[0].show_onboarding;
-  }).except(initFail);
+        SWBrijj.tblm('ownership.company_issue').then(function (data) {
+            if (Object.keys(data).length > 0) {
+                $scope.captablestart = "Edit";
+            }
+        });
 
-  $scope.close = function() {
-    $scope.onboarding = false;
-    SWBrijj.procm('account.onboarding_update', false);
-  };
+        if ($routeParams.msg) {
+            if ($routeParams.msg == "resetPassword") {
+                $scope.$emit("notification:success", "You have successfully changed your password.");
+            }
+        }
 
- $scope.activity = [];
-  SWBrijj.tblm('global.get_company_activity').then(function(data) {
-      $scope.activity = data;
-      if ($scope.activity.length == 0) {
-          $scope.noActivity = true;
-      }
-  });
+        $scope.company = navState.name;
+        SWBrijj.tblm('account.onboarding').then(function(x) {
+            $scope.onboarding = x[0].show_onboarding;
+        }).except(initFail);
 
-  $scope.activityOrder = function(card) {
-        return -card.time;
-  };
-}]);
+        $scope.close = function() {
+            $scope.onboarding = false;
+            SWBrijj.procm('account.onboarding_update', false);
+        };
+
+        $scope.activity = [];
+        SWBrijj.tblm('global.get_company_activity').then(function(data) {
+            $scope.activity = data;
+            if ($scope.activity.length == 0) {
+                $scope.noActivity = true;
+            }
+        });
+
+        $scope.activityOrder = function(card) {
+            return -card.time;
+        };
+    }]);
 
 app.controller('InvestorCtrl', ['$scope','$rootScope','$location', '$route','$routeParams', 'SWBrijj', 'navState',
-  function($scope, $rootScope, $location, $route, $routeParams, SWBrijj, navState) {
+    function($scope, $rootScope, $location, $route, $routeParams, SWBrijj, navState) {
 
-    if (navState.role == 'issuer') {
-        $location.path('/company');
-        return;
-    }
+        if (navState.role == 'issuer') {
+            $location.path('/company');
+            return;
+        }
 
-  if ($routeParams.msg) {
-    if ($routeParams.msg == "resetPassword") {
-      $scope.$emit("notification:success", "You have successfully changed your password.");
-    }
-  }
-  //$scope.company = $routeParams.company;
-  $scope.company = navState.name;
-  $scope.activity = [];
-  SWBrijj.tblm('global.get_investor_activity').then(function(data) {
-      $scope.activity = data;
-      if ($scope.activity.length == 0) {
-          $scope.noActivity = true;
-      }
-  }).except(function(msg) {
-        // console.log(msg.message);
-      });
+        if ($routeParams.msg) {
+            if ($routeParams.msg == "resetPassword") {
+                $scope.$emit("notification:success", "You have successfully changed your password.");
+            }
+        }
+        //$scope.company = $routeParams.company;
+        $scope.company = navState.name;
+        $scope.activity = [];
+        SWBrijj.tblm('global.get_investor_activity').then(function(data) {
+            $scope.activity = data;
+            if ($scope.activity.length == 0) {
+                $scope.noActivity = true;
+            }
+        }).except(function(msg) {
+                // console.log(msg.message);
+            });
 
-  $scope.activityOrder = function(card) {
-        return -card.time;
-  };
-}]);
+        $scope.activityOrder = function(card) {
+            return -card.time;
+        };
+    }]);
 
 app.controller('HomeCtrl',['$scope','$route', 'SWBrijj', function($scope, $route, SWBrijj) {
-  SWBrijj.tbl('account.companies').then(function(x) {
-    // console.log(x);
-    if (x.length > 0) { //User is a CEO of a company
-      document.location.href="company";
-    } else {
-      SWBrijj.tblm('account.invested_companies').then(function(x) {
-        document.location.href=x[0]['company'];
-      })
-    }
-  });  
+    SWBrijj.tbl('account.companies').then(function(x) {
+        // console.log(x);
+        if (x.length > 0) { //User is a CEO of a company
+            document.location.href="company";
+        } else {
+            SWBrijj.tblm('account.invested_companies').then(function(x) {
+                document.location.href=x[0]['company'];
+            })
+        }
+    });
 }]);
 
 /*
-function initPage($scope, x, row) {
-  if(typeof(row)==='undefined') row = 1;
-  var y = x[0]; // the fieldnames
-  var z = x[row]; // the values
-  
-  for(var i=0;i<y.length;i++) { if (z[i] !== null) { $scope[y[i]]=z[i]; } }
-  $scope.$apply();
-} */
+ function initPage($scope, x, row) {
+ if(typeof(row)==='undefined') row = 1;
+ var y = x[0]; // the fieldnames
+ var z = x[row]; // the values
+
+ for(var i=0;i<y.length;i++) { if (z[i] !== null) { $scope[y[i]]=z[i]; } }
+ $scope.$apply();
+ } */
 
 function initFail(x) {
-  void(x);
-  // console.log('I would have redirected to login'); // document.location.href='/login';
+    void(x);
+    // console.log('I would have redirected to login'); // document.location.href='/login';
 }
 
 /************************************************************
@@ -150,7 +159,7 @@ angular.module('HomeApp').filter('description', function() {
         if (type == "ownership") {
             if (activity == "received") return "Ownership Table sent to " + person;
             else if (activity == "viewed") return "Ownership Table viewed by "+person;
-          else return "Something with Ownership Table";
+            else return "Something with Ownership Table";
         }
         else {
             var document = ac.docname;
@@ -186,7 +195,7 @@ angular.module('HomeApp').filter('investordescription', function() {
         if (type == "ownership") {
             if (activity == "received") return "You received " + company + "'s captable";
             else if (activity == "viewed") return "You viewed " + company + "'s captable";
-          else return "Something happened with "+company +"'s captable";
+            else return "Something happened with "+company +"'s captable";
         }
         else if (type == "document") {
             var document = ac.docname;
