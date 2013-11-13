20fbe20b2bfbab9a31b9f7a085779fad6f7833b0
Robert Walport <robertwalport@gmail.com>
2013-11-13 14:38:37 -0500

    another go at this usersnap lark

--- cmn/nav.js
+++ cmn/nav.js
@@ -143,18 +143,7 @@ navm.controller('NavCtrl', ['$scope', '$route', '$rootScope', 'SWBrijj', '$q', '
             if ($rootScope.navState.role == "issuer") {
                 Intercom('boot', {email:$rootScope.navState.userid, user_hash: $rootScope.navState.userhash,  app_id: "e89819d5ace278b2b2a340887135fa7bb33c4aaa", company:{id: $rootScope.navState.company, name: $rootScope.navState.name}});
             }
-            $(document).ready(function() {
-                $.usersnap({
-                    apiKey: 'efc0c5d1-c4c5-4260-8cea-b16a2406faef',
-                    valign: 'bottom',
-                    halign: 'right',
-                    tools: ["pen", "highlight", "note"],
-                    lang: 'en',
-                    commentBox: true,
-                    emailBox: true
-                });
-                $.usersnap('emailbox', String($rootScope.navState.userid));
-            });
+            UserSnap.setEmailBox($rootScope.navState.userid);
         }).except(function (ignore) {
                 void(ignore);
                 $scope.navState={}; // ?
