027ae756d5400d9db5a811038973db42f6700f3d
Robert Walport <robertwalport@gmail.com>
2013-11-15 17:52:18 -0500

    added josh template and readded regular expression for emails on the home page

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.controller('IndexCtrl', ['$scope','$rootScope','$route','$location', '$route
   }*/
 
   $scope.fieldCheck = function() {
-    return !($scope.user.email);
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test($scope.user.email);
   };
 
   $scope.toggle = true;
