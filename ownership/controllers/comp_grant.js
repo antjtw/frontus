562e81746bee2f7b0d0f170dbd8f9dbf48e17921
Robert Walport <robertwalport@gmail.com>
2013-12-13 10:23:06 -0500

    only equity rounds can be underlying for options

--- ownership/controllers/comp_grant.js
+++ ownership/controllers/comp_grant.js
@@ -26,6 +26,7 @@ var grantController = function ($scope, $rootScope, $parse, $location, SWBrijj, 
     $scope.tf = ["yes", "no"];
     $scope.issues = [];
     $scope.issuekeys = [];
+    $scope.equityissues = [];
     $scope.possibleActions = ['exercised', 'forfeited'];
 
     // False is edit mode, true is view mode
@@ -76,6 +77,9 @@ var grantController = function ($scope, $rootScope, $parse, $location, SWBrijj, 
                         $scope.issues.push($scope.allissues[i]);
                     }
                     $scope.issuekeys.push($scope.allissues[i].issue);
+                    if ($scope.allissues[i].type == "Equity") {
+                        $scope.equityissues.push($scope.allissues[i].issue);
+                    }
                 }
 
                 // Assign the grants to the respective transactions
