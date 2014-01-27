7d282657b6329ea34ea405b57433418a4ef4b205
Robert Walport <robertwalport@gmail.com>
2014-01-27 15:22:01 -0500

    even more logging

--- ownership/controllers/comp_captable.js
+++ ownership/controllers/comp_captable.js
@@ -609,7 +609,7 @@ var captableController = function ($scope, $rootScope, $location, $parse, SWBrij
                 if (issue['vestingbegins'] == undefined) {
                     var vestcliffdate = null
                 }
-
+                console.log("saving");
                 SWBrijj.proc('ownership.update_issue', issue['key'], issue['type'], d1, issue['issue'], parseFloat(issue['premoney']), parseFloat(issue['postmoney']), parseFloat(issue['ppshare']), parseFloat(issue['totalauth']), partpref, issue.liquidpref, issue['optundersec'], parseFloat(issue['price']), parseFloat(issue['terms']), vestcliffdate, parseFloat(issue['vestcliff']), issue['vestfreq'], issue['debtundersec'], parseFloat(issue['interestrate']), issue['interestratefreq'], parseFloat(issue['valcap']), parseFloat(issue['discount']), parseFloat(issue['term']), dragalong, tagalong, common).then(function (data) {
                     console.log($scope.issues.length);
                     if ($scope.issues.length == 1) {
