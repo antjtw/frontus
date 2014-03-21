f3ad6851f43c80e215bd3043344507a73518bfa1
Robert Walport <robertwalport@gmail.com>
2014-03-21 17:12:59 -0400

    created a new document service and wiped out a lot of code

---
+++ documents/documentservice.js
@@ -0,0 +1,38 @@
+var docs = angular.module('docServices', []);
+
+// Captable functions for basic mathematics. Should be expanded by peeling some of the reusable pieces out of the controller.
+docs.service('basics', function () {
+
+    this.eventRank = function (ev) {
+        // signed or rejected can come either before or after each other depending on chronological ordering.
+        // ambiguity is resolve in $scope.compareEvents
+        switch (ev.activity) {
+            case "retracted":
+                return 10;
+            case "finalized":
+                return 7;
+            case "countersigned":
+                return 6;
+            case "signed":
+            case "rejected":
+                return 4;
+            case "viewed":
+                return 3;
+            case "received":
+                return 2;
+            case "uploaded":
+                return 1;
+            default:
+                return 0;
+        }
+    };
+
+    this.isCompleteSigned = function(version) {
+        return version.signature_flow>0 && version.when_finalized;
+    };
+
+    this.isCompleteViewed = function(version) {
+        return version.signature_flow===0 && version.last_viewed;
+    };
+
+});
