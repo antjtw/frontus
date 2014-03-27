55b1ea310b586671490039ee711a6bb9745e039d
Robert Walport <robertwalport@gmail.com>
2014-03-27 15:40:27 -0400

    adding the new x

--- documents/js/Documents.js
+++ documents/js/Documents.js
@@ -818,7 +818,7 @@ docs.controller('DocumentViewController', ['$scope', '$rootScope', '$compile', '
         $scope.voidAction = function(confirm, message) {
             $scope.processing = true;
             if (message == "Explain the reason for rejecting this document.") {
-                message = "";
+                message = " ";
             }
             SWBrijj.document_investor_void($scope.docId, confirm, message).then(function(data) {
                 if (confirm == 1) {
