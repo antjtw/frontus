f370d191986f76e198120ba02779a22c7fb49b73
brianru <brianrubinton@gmail.com>
2014-04-04 08:31:53 -0400

    removed new company modal -- direct button to new-company page

--- cmn/nav.js
+++ cmn/nav.js
@@ -435,31 +435,6 @@ navm.controller('NavCtrl', ['$scope', '$route', '$rootScope', 'SWBrijj', '$q', '
             return notifications
         };
 
-        $scope.addCompanyModalUp = function() {
-            $scope.addCompanyModal = true;
-        };
-
-        $scope.addCompanyModalClose = function() {
-            $scope.addCompanyModal = false;
-        };
-
-        $scope.createNewCompany = function(name) {
-            if (name.length > 0) {
-                SWBrijj.procm('account.new_company', name).then(function (new_comp_id) {
-                    var company = {"company": new_comp_id[0].new_company, "role": "issuer"};
-                    $scope.switchCandP(company, "/app/home/company?cc");
-                });
-            }
-            else {
-                $scope.addCompanyModal = false;
-            }
-        };
-
-        $scope.opts = {
-            backdropFade: true,
-            dialogFade: true
-        };
-
         var idleTime = 0;
 
         function timerIncrement() {
