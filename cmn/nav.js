7a444de098a00e84dc070ccfda3ecb50e58e09c0
Robert Walport <robertwalport@gmail.com>
2014-04-16 12:45:58 -0400

    backend not passing enough data so removed when_attempted for now

--- cmn/nav.js
+++ cmn/nav.js
@@ -102,7 +102,7 @@ navm.controller('NavCtrl', ['$scope', '$route', '$rootScope', 'SWBrijj', '$q', '
         $scope.$on('$routeChangeSuccess', function(current, previous) {
             navState.path = document.location.pathname;
             if ($scope.plan) {
-                Intercom('update', {company:  {'plan' : $filter('billingPlans')($scope.plan.plan), 'changed_at' : parseInt(Date.parse($scope.plan.when_attempted).getTime()/1000)}});
+                Intercom('update', {company:  {'plan' : $filter('billingPlans')($scope.plan.plan)}});
             }
         });
 
@@ -142,17 +142,18 @@ navm.controller('NavCtrl', ['$scope', '$route', '$rootScope', 'SWBrijj', '$q', '
             $scope.plan = p;
             if (p && p.plan != '000' && ((p.customer_id !== null && p.cc_token !== null) || (p.when_request != null && p.when_attempted == null))) {
                 $rootScope.persistentNotification = false;
-                Intercom('update', {company:  {'plan' : $filter('billingPlans')(p.plan), 'changed_at' : parseInt(Date.parse(p.when_attempted).getTime()/1000)}});
+                console.log(p);
+                Intercom('update', {company:  {'plan' : $filter('billingPlans')(p.plan)}});
             } else {
                 $rootScope.persistentNotification = true;
                 if (p) {
                     if (p.status) {
                         $rootScope.paymentmessage = "We've had a problem with your payment. Click here to update your card.";
-                        Intercom('update', {company:  {'plan' : $filter('billingPlans')(p.plan) + " failed", 'changed_at' : parseInt(Date.parse(p.when_attempted).getTime()/1000)}});
+                        Intercom('update', {company:  {'plan' : $filter('billingPlans')(p.plan) + " failed"}});
                     }
                     else {
                         $rootScope.paymentmessage = "You've cancelled your account, click here to start a new payment plan.";
-                        Intercom('update', {company:  {'plan' : $filter('billingPlans')(p.plan) + " cancelled", 'changed_at' : parseInt(Date.parse(p.when_attempted).getTime()/1000)}});
+                        Intercom('update', {company:  {'plan' : $filter('billingPlans')(p.plan) + " cancelled"}});
                     }
                 }
                 else {
