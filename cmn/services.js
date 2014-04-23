379e83502e802f8e84bb92e06435293a0be482f5
brianru <brianrubinton@gmail.com>
2014-04-23 19:45:37 -0400

    working through a self-contained myPayments service

--- cmn/services.js
+++ cmn/services.js
@@ -71,3 +71,89 @@ service.factory('payments', function($http, SWBrijj) {
 
     return s;
 });
+
+service.service('myPayments', function(payments) {
+    this.data = {};
+    var d = {};
+    // TODO these must return promises
+    // so where are results accumulated/published?
+    var loadPlans = function() {
+            return payments
+                    .available_plans()
+                    .then(function(x)
+                    {
+                        d.plans = [];
+                        angular.forEach(x, function(p) {
+                            d.plans.push(p.plan);
+                        });
+                        d.recommendedPlan
+                            = "00" + Math.max(parseInt(d.plans, 10));
+                        return null;
+                    });
+        },
+        loadUsage = function() {
+            return payments
+                    .usage_details()
+                    .then(function(x)
+                    {
+                        if (x.length === 0) {
+                            loadSpecifiedUsage(d.recommendedPlan);
+                        } else {
+                            d.usage = x[0];
+                        }
+                        return x;
+                    });
+        },
+        loadSpecifiedUsage = function(p) {
+            return payments
+                    .usage_grid(p)
+                    .then(function(x)
+                    {
+                        d.usage = x;
+                    });
+        },
+        loadUserPaymentData = function() {
+            return payments
+                    .my_data()
+                    .then(function(x)
+                    {
+                        /*
+                        if (data.length > 0) {
+                            $scope.billing.currentPlan =
+                                $scope.selectedPlan = data[0].plan || '000';
+                            $scope.billing.customer_id = data[0].customer_id;
+                            $scope.billing.payment_token = data[0].cc_token;
+                            $scope.load_invoices();
+                            payments.get_customer($scope.billing.customer_id)
+                            .then(function(x) {
+                                $scope.billing.current_card = x.data.cards.data[0];
+                                $scope.openModalsFromURL();
+                            });
+                        } else {
+                            if (parseInt($scope.billing.recommendedPlan, 10) > 2) {
+                                $scope.selectedPlan = $scope.billing.recommendedPlan;
+                            } else {
+                                $scope.selectedPlan = '002';
+                            }
+                            $scope.openModalsFromURL();
+                        }
+                        
+                        */
+                    });
+        },
+        loadCustomerInvoices = function() {
+            return payments.get_invoices();
+        },
+        broadcastResults = function() {
+            this.data = d;
+            console.log(this.data);
+        };
+        
+
+    loadPlans()
+        .then( loadUsage )
+        .then( loadUserPaymentData )
+        .then( loadCustomerInvoices )
+        .then( broadcastResults );
+
+});
