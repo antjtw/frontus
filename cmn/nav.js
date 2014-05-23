d647ef5a1de67d2169be264497ac49af00e59572
elizabethkilson <elizabethkilson@gmail.com>
2014-05-23 10:59:48 -0400

    Finished making dropbox features secure.

--- cmn/nav.js
+++ cmn/nav.js
@@ -716,8 +716,6 @@ navm.controller('NavCtrl',
         };
         SWBrijj.procm('oauth.token_num').then(function(data) {
             $scope.access_token = data[0]['token_num'];
-            console.log(data[0]);
-            console.log($scope.access_token);
         });
 
         //I don't love this but it works, should probably make a directive.
