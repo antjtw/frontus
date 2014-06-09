17d157ef279b485b611a4d86bbaf0a7b928d6543
Robert Walport <robertwalport@gmail.com>
2014-06-09 11:50:12 -0400

    working through the data

--- features/directives.js
+++ features/directives.js
@@ -40,18 +40,16 @@ app.directive('d3Discount', ['d3', function(d3) {
 
                 var color = d3.scale.category10();
                 var maxy = 0;
-                angular.forEach(data[0], function(value, key) {
-                    maxy = d3.max(value) > maxy ? d3.max(value) : maxy;
+                var maxx = 0;
+                var minx = Infinity;
+                angular.forEach(data, function(value) {
+                    maxy = value.y > maxy ? value.y : maxy;
+                    maxx = value.x > maxx ? value.x : maxx;
+                    minx = value.x < minx ? value.x : minx;
                 });
-                var maxx = data[1][4];
-                var minx = data[1][0];
                 var y = d3.scale.linear().domain([0, maxy]).range([height, 0]);
                 var x = d3.scale.linear().domain([minx, maxx]).range([0, width]);
 
-
-                var line = d3.svg.line()
-                    .x(function(d) { return x(d[1]);})
-                    .y(function(d) { return y(d[0]); });
 
                 var xAxis = d3.svg.axis()
                     .scale(x)
@@ -84,50 +82,47 @@ app.directive('d3Discount', ['d3', function(d3) {
                 gy.selectAll("text")
                     .attr("display", "none");
 
-                angular.forEach(data[0], function(value, key) {
-                    //create the lines
-                    var points = [];
-                    var i = 0;
-                    angular.forEach(value, function(y) {
-                        points.push([y, data[1][i]]);
-                        i++;
-                    });
-                    var linecolour = color(points);
-
-                    svg.append("path")
-                        .attr("d", line(points))
-                        .style("stroke", linecolour)
-                        .style("stroke-width", 1)
-                        .style("fill", "transparent");
-
-                    angular.forEach(points, function(point) {
-                        svg.append("circle")
-                            .data(point)
-                            .attr("cx", function(d) {
-                                return x(point[1]);
-                            })
-                            .attr("cy", function(d) {
-                                return y(point[0]);
-                            })
-                            .attr("r", 3)
-                            .on("mouseover", function(d) {
-                                div.transition()
-                                    .duration(200)
-                                    .style("opacity", .9);
-                                div.html(String(d))
-                                    .style("left", (d3.event.pageX) + "px")
-                                    .style("top", (d3.event.pageY - 28) + "px");
-                            })
-                            .on("mouseout", function(d) {
-                                div.transition()
-                                    .duration(200)
-                                    .style("opacity", 0);
-                            })
-                            .style("stroke", linecolour)
-                            .style("fill", "white");
-                    })
-
+                var points = [];
+                var i = 0;
+                angular.forEach(data, function(point) {
+                    points.push([point.y, point.x]);
+                    i++;
                 });
+                var linecolour = color(points);
+
+                svg.append("path")
+                    .attr("d", line(points))
+                    .style("stroke", linecolour)
+                    .style("stroke-width", 1)
+                    .style("fill", "transparent");
+
+                angular.forEach(points, function(point) {
+                    svg.append("circle")
+                        .data(point)
+                        .attr("cx", function(d) {
+                            return x(point[1]);
+                        })
+                        .attr("cy", function(d) {
+                            return y(point[0]);
+                        })
+                        .attr("r", 3)
+                        .on("mouseover", function(d) {
+                            div.transition()
+                                .duration(200)
+                                .style("opacity", .9);
+                            div.html(String(d))
+                                .style("left", (d3.event.pageX) + "px")
+                                .style("top", (d3.event.pageY - 28) + "px");
+                        })
+                        .on("mouseout", function(d) {
+                            div.transition()
+                                .duration(200)
+                                .style("opacity", 0);
+                        })
+                        .style("stroke", linecolour)
+                        .style("fill", "white");
+                });
+
 
                 svg.append("text")
                     .attr("class", "x label")
