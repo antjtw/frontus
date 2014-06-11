c5b4f260fd968666c941a86a0c8681e54153274c
Bert Hartmann <BertHartm@gmail.com>
2014-06-11 11:11:59 -0400

    fix casing and tensing in activity feed retraction

--- cmn/activity/activity.js
+++ cmn/activity/activity.js
@@ -204,11 +204,11 @@ active.filter('description', function() {
             else if (activity == "rejected") return "Signature rejected by " +person;
             else if (activity == "countersigned") return "Countersigned by " + person;
             else if (activity == "finalized") return "Approved by " + person;
-            else if (activity == "Retracted") return "Retracted by " + person;
+            else if (activity == "retracted") return "Retracted by " + person;
             else if (activity == "void requested") return "Void request sent to " +person;
             else if (activity == "void accepted") return "Void accepted by " + person;
             else if (activity == "void rejected") return "Void rejected by " + person;
-            else return activity + "ed Document";
+            else return activity + " Document";
         }
     }
 });
