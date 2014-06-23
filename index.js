6a9f3dcebea6cb2ccda5450eb5b4d86200c085e6
Robert Walport <robertwalport@gmail.com>
2014-06-23 13:48:54 -0400

    new messages in the typing homepage

--- index.js
+++ index.js
@@ -69,18 +69,21 @@ app.controller('IndexCtrl', ['$scope','$rootScope','$route','$location', '$route
             if ("I want to keep track of our investors".startsWith(currentValue)) {
                 ga('send', 'event', 'homepage', 'hero-click', 'I want to keep track of our investors');
                 document.location.href = '/features/cap';
-            } else if ("I want to model our company value".startsWith(currentValue)) {
-                ga('send', 'event', 'homepage', 'hero-click', 'I want to model our company value');
+            } else if ("I want to build a cap table".startsWith(currentValue)) {
+                ga('send', 'event', 'homepage', 'hero-click', 'I want to build a cap table');
                 document.location.href = '/features/cap';
-            } else if ("I want to share documents securely".startsWith(currentValue)) {
-                ga('send', 'event', 'homepage', 'hero-click', 'I want to share documents securely');
+            } else if ("I want to share documents for e-signature".startsWith(currentValue)) {
+                ga('send', 'event', 'homepage', 'hero-click', 'I want to share documents for e-signature');
                 document.location.href = '/features/doc';
-            } else if ("I want to have documents e-signed".startsWith(currentValue)) {
-                ga('send', 'event', 'homepage', 'hero-click', 'I want to have documents e-signed');
-                document.location.href = '/features/doc';
+            } else if ("I want to make sense of convertible notes".startsWith(currentValue)) {
+                ga('send', 'event', 'homepage', 'hero-click', 'I want to make sense of convertible notes');
+                document.location.href = '/features/convertible-notes';
             } else if ("I want to grant options hassle free".startsWith(currentValue)) {
                 ga('send', 'event', 'homepage', 'hero-click', 'I want to grant options hassle free');
                 document.location.href = '/features/#options';
+            } else if ("send updates to my investors".startsWith(currentValue)) {
+                ga('send', 'event', 'homepage', 'hero-click', 'send updates to my investors');
+                document.location.href = '/features/#message';
             }
         }
 
