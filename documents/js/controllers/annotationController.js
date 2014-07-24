7d829cad255197b1e8a03d6a9ed17b0ceae8f8c3
Bert Hartmann <BertHartm@gmail.com>
2014-07-24 10:48:59 -0400

    move enum type annotations select2 box to the blue popup

--- documents/js/controllers/annotationController.js
+++ documents/js/controllers/annotationController.js
@@ -362,10 +362,12 @@ function annotationController($scope, $rootScope, $element, $document, Annotatio
 
     $scope.enumBoxMode = function() {
         // whether the annotation box should be a select2 dropdown
+        // note that issuers see it in the blue popup, so it's always false for them
         return $scope.annot.type_info &&
                $scope.annot.type_info.typename == 'enum' &&
                $scope.active.annotation == $scope.annot &&
-               $scope.annot.forRole(navState.role);
+               $scope.annot.forRole(navState.role)&&
+               navState.role == 'investor';
     };
 
     $scope.dateBoxMode = function() {
