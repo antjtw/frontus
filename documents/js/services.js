075be3f863973ecca3f7823b1857991be92ee954
Robert Walport <robertwalport@gmail.com>
2014-07-25 11:32:35 -0400

    styling and ordering the select2 dropdown

--- documents/js/services.js
+++ documents/js/services.js
@@ -271,6 +271,22 @@ docs.service('Documents', ["Annotations", "SWBrijj", "$q", "$rootScope", functio
             });
             return num;
         },
+        dropdownDisplay: function(type) {
+            if (type.required) {
+                return type.display + " (Required)"
+            } else {
+                return type.display
+            }
+        },
+        annotationOrder: function(type) {
+            if (type.required) {
+                return 0
+            } else if (type.required == false) {
+                return 1000
+            } else {
+                return 2000
+            }
+        },
         annotable: function(role) {
             if (role == "investor")
                 return this.investorCanAnnotate();
