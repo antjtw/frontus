85b409c9be17d6d1205fab28b96700ae671fce04
Bert Hartmann <BertHartm@gmail.com>
2014-08-11 17:46:44 -0400

    fix issue with document viewer not setting overrides

--- documents/js/controllers/annotationController.js
+++ documents/js/controllers/annotationController.js
@@ -400,17 +400,18 @@ function annotationController($scope, $rootScope, $element, $document, Annotatio
 
     // create variables we can bind to for placeholder and value
     // depending on whether we're using the annotation proper, or an override value
+    $scope.current = {};
     $scope.$watch('annot.type_info.display', function(display) {
         if ((!$scope.prepareFor) || ($scope.annot.val.length === 0)) {
-            $scope.current_placeholder = display;
+            $scope.current.placeholder = display;
         }
     });
     if ($scope.prepareFor) {
         $scope.$watch('annot.val', function(val) {
             if (val.length > 0) {
-                $scope.current_placeholder = val;
+                $scope.current.placeholder = val;
             } else {
-                $scope.current_placeholder = $scope.annot.type_info.display;
+                $scope.current.placeholder = $scope.annot.type_info.display;
             }
         });
         $scope.$watch(function() {
@@ -422,9 +423,9 @@ function annotationController($scope, $rootScope, $element, $document, Annotatio
                 return "";
             }
         }, function(override) {
-            $scope.current_val = override;
+            $scope.current.val = override;
         });
-        $scope.$watch('current_val', function(val, old_val) {
+        $scope.$watch('current.val', function(val, old_val) {
             if (val == old_val) {
                 // don't run the first time
                 return;
@@ -437,12 +438,11 @@ function annotationController($scope, $rootScope, $element, $document, Annotatio
                     $scope.doc.preparedFor[$scope.prepareFor].overrides[$scope.annot.id].val = val;
                 }
             } else {
-                var hash;
                 if (!$scope.doc.preparedFor) {
                     $scope.doc.getPreparedFor();
                 }
                 if (!$scope.doc.preparedFor[$scope.prepareFor]) {
-                    hash = $scope.doc.addPreparedFor($scope.prepareFor);
+                    var hash = $scope.doc.addPreparedFor($scope.prepareFor);
                     hash.overrides[$scope.annot.id] = {id: $scope.annot.id, val: val};
                 } else {
                     $scope.doc.preparedFor[$scope.prepareFor].overrides[$scope.annot.id] = {id: $scope.annot.id, val: val};
@@ -451,11 +451,11 @@ function annotationController($scope, $rootScope, $element, $document, Annotatio
         });
     } else {
         $scope.$watch('annot.val', function(val) {
-            if ($scope.current_value != val) {
-                $scope.current_value = val;
+            if ($scope.current.val != val) {
+                $scope.current.val = val;
             }
         });
-        $scope.$watch('current_val', function(val) {
+        $scope.$watch('current.val', function(val) {
             if ($scope.annot.val != val) {
                 $scope.annot.val = val;
             }
