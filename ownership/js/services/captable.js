4a96b71d543d0f40ffeb2fdd79ec864954fb12ef
brianru <brianrubinton@gmail.com>
2014-08-26 15:32:48 -0400

    update captable controller when deleting security from security details directive

--- ownership/js/services/captable.js
+++ ownership/js/services/captable.js
@@ -742,6 +742,7 @@ function($rootScope, calculate, SWBrijj, $q, attributes, History) {
             if (res > 0) {
                 $rootScope.$emit("notification:success",
                     "Security deleted");
+                $rootScope.$broadcast("deleteSecurity");
 
                 var idx = captable.securities.indexOf(sec);
                 if (idx !== -1) { captable.securities.splice(idx, 1); }
@@ -869,8 +870,13 @@ function($rootScope, calculate, SWBrijj, $q, attributes, History) {
                 return el.name==sec && el.attrs.security_type;
             })[0];
         initAttrs(tran, sec_obj.attrs.security_type, kind);
+        /*
         tran.attrs.security = sec;
         tran.attrs.security_type = sec_obj.attrs.security_type;
+        */
+        angular.forEach(tran.attrs, function(value, key) {
+            if (sec_obj.key) tran.attrs.key = sec_obj.key;
+        });
         if (tran.attrs.hasOwnProperty('investor'))
         {
             tran.attrs.investor = inv;
