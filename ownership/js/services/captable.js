a8e74917892b6f731fcd326d87aecfca763cf804
Robert Walport <robertwalport@gmail.com>
2014-09-19 12:25:30 -0400

    saving timezone fix

--- ownership/js/services/captable.js
+++ ownership/js/services/captable.js
@@ -1190,21 +1190,19 @@ function($rootScope, navState, calculate, SWBrijj, $q, attributes, History, $fil
                 delete tran.attrs[key];
             }
         }
-        console.log(tran.effective_date);
-        console.log(typeof tran.effective_date);
         if (typeof tran.effective_date == 'object') {
             tran.effective_date = calculate.castDateString(tran.effective_date, $rootScope.settings.shortdate);
         }
         SWBrijj.procm('_ownership.save_transaction',
                       JSON.stringify(tran))
         .then(function(new_entries) {
+            tran.effective_date = Date.parse(tran.effective_date);
             if (new_entries.length < 1)
             {
                 console.log("Error: no ledger entries");
                 return;
             }
             var transaction = new_entries.splice(0, 1)[0].transaction;
-            //console.log("new ledger", new_entries.length, new_entries, JSON.stringify(tran));
             var spliced = [];
             for (var new_entry in new_entries)
             {
