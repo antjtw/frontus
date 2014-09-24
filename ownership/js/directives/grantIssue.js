2c3b44326e1ff16b62b5c0f328ef58e8b4147b96
Bert Hartmann <BertHartm@gmail.com>
2014-09-24 12:36:05 -0400

    allow creating a new options issue from the grants screen

--- ownership/js/directives/grantIssue.js
+++ ownership/js/directives/grantIssue.js
@@ -17,6 +17,16 @@ app.directive('grantIssue', [function() {
             $scope.issueSelectOptions = {
                 data: [],
                 placeholder: "Pick an option security",
+                createSearchChoice: function(new_name) {
+                    var sec = captable.nullSecurity();
+                    sec.name = new_name;
+                    console.log(sec);
+                    return {
+                        id: new_name,
+                        text: new_name,
+                        issue: sec
+                    };
+                }
             };
 
             $scope.$watchCollection('issues', function(issues) {
@@ -38,6 +48,20 @@ app.directive('grantIssue', [function() {
             $scope.issue = grants.issue;
 
             $scope.$watch('selected.issue', function(new_issue, old_issue) {
+                if (new_issue === null && typeof(old_issue) === "string" && old_issue !== "") {
+                    // not sure why, but select2 is returning null values for new securities
+                    // regardless, we can get the name out of old_issue and build a security with that
+                    var new_sec = captable.newSecurity();
+                    new_sec.transactions[0].attrs.security = old_issue;
+                    new_sec.transactions[0].attrs.security_type = "Option";
+                    captable.addSecurity(new_sec);
+                    $scope.selected.issue = {
+                        id: old_issue,
+                        text: old_issue,
+                        issue: new_sec
+                    };
+                    new_issue = $scope.selected.issue;
+                }
                 if (new_issue && typeof(new_issue) !== "string" && (!grants.issue[0] || new_issue.text !== grants.issue[0].name)) {
                     grants.setIssue(new_issue.issue);
                 }
