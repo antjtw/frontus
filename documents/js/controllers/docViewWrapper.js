1456cea6b0828a12bbcfcca538a9e377d36d80a2
Robert Walport <robertwalport@gmail.com>
2014-10-03 18:07:54 -0400

    using select2 and the investor service

--- documents/js/controllers/docViewWrapper.js
+++ documents/js/controllers/docViewWrapper.js
@@ -1,8 +1,8 @@
 'use strict';
 
 app.controller('DocumentViewWrapperController', ['$scope', '$routeParams', '$route', '$rootScope', '$timeout', '$location', 'SWBrijj',
-        'navState', 'Annotations', 'Documents', '$q', 'basics', 'ShareDocs',
-    function($scope, $routeParams, $route, $rootScope, $timeout, $location, SWBrijj, navState, Annotations, Documents, $q, basics, ShareDocs) {
+        'navState', 'Annotations', 'Documents', '$q', 'basics', 'ShareDocs', 'Investor',
+    function($scope, $routeParams, $route, $rootScope, $timeout, $location, SWBrijj, navState, Annotations, Documents, $q, basics, ShareDocs, Investor) {
         $scope.investor_attributes = {}; // need investor attributes to be defined in this scope so we can save them
         $scope.nextAnnotationType = 'text';
 
@@ -450,8 +450,13 @@ app.controller('DocumentViewWrapperController', ['$scope', '$routeParams', '$rou
                     certificatedocument[0].security = doc.preparedFor[investor].overrides[note.id];
                 }
             });
+            var emailto = $scope.doc.row.email;
+            if (typeof(inv) != "string") {
+                emailto = $scope.doc.row.email.id;
+            }
+            console.log(emailto);
             SWBrijj.document_multishare(
-                    $scope.doc.row.email.toLowerCase(),
+                    emailto.toLowerCase(),
                     JSON.stringify($scope.certificatedocument),
                     "",
                     "22 November 2113"
@@ -462,8 +467,26 @@ app.controller('DocumentViewWrapperController', ['$scope', '$routeParams', '$rou
                 });
         };
 
+        $scope.rowSelect2Options = {
+            data: function() {
+                return {
+                    results: Investor.investors
+                };
+            },
+            createSearchChoice: Investor.createSearchChoice,
+            placeholder: 'Pick name or type email'
+        };
+
+        $scope.updateSendRow = function(inv) {
+            console.log(inv);
+            if (typeof(inv) === "string") {
+                // select2-ui sets string and then object, generate the object from the string
+                $scope.doc.row.email = Investor.createInvestorObject(inv);
+            }
+        };
+
         $scope.formCheck = function() {
-            return $scope.doc.row && $scope.doc.row.email.length > 0 && emailRegExp.test($scope.doc.row.email);
+            return $scope.doc.row && $scope.doc.row.email && $scope.doc.row.email.id.length > 0 && emailRegExp.test($scope.doc.row.email.id);
         };
 
         $scope.prepareable = function() {
