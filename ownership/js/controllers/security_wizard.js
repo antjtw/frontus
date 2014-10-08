ec8e098f1a5f2760d8ffbc324271b9c49fe213c0
Robert Walport <robertwalport@gmail.com>
2014-10-08 14:15:21 -0400

    maintaining state and creating correctly

--- ownership/js/controllers/security_wizard.js
+++ ownership/js/controllers/security_wizard.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.controller('createSecurity',
-    ["$scope", "Documents", "captable", "$location", function($scope, Documents, captable, $location){
+    ["$scope", "Documents", "captable", "$location", "$routeParams", "$rootScope", function($scope, Documents, captable, $location, $routeParams, $rootScope){
         $scope.step = 1;
         $scope.rawissue = {};
         $scope.issue = [];
@@ -10,8 +10,22 @@ app.controller('createSecurity',
             $scope.rawissue.type = type;
         };
 
+        $rootScope.$watchCollection(function() {
+            return captable.getCapTable().securities;
+        }, function(securities) {
+            securities.some(function(sec) {
+                if (sec.name == $routeParams.issue) {
+                    $scope.issue = [sec];
+                    $scope.rawissue.name = $scope.issue[0].transactions[0].attrs.security;
+                    $scope.rawissue.type = $scope.issue[0].transactions[0].attrs.security_type;
+                    $scope.step = 2;
+                }
+            });
+        });
+
         $scope.changeStep = function(newstep) {
             if (newstep == 2) {
+                $location.search('issue', $scope.rawissue.name).replace();
                 $scope.issue.push(captable.newSecurity());
                 $scope.issue[0].transactions[0].attrs.security = $scope.rawissue.name;
                 $scope.issue[0].transactions[0].attrs.security_type = $scope.rawissue.type;
